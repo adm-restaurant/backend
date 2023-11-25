38277ceb6f043bbe42826ef5da99f510165d78a6
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-25 03:42:34 -0300

    feat: create util for security

---
+++ src/main/java/com/admrestaurant/backend/common/SecurityUtils.java
@@ -0,0 +1,20 @@
+package com.admrestaurant.backend.common;
+
+import com.admrestaurant.backend.entities.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtils {
+    public static String getCurrentUsername() {
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User) {
+            return ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        } 
+        throw new RuntimeException("User not found");
+    }
+
+    public static Long getCurrentUserId() {
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User) {
+            return ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        }
+        throw new RuntimeException("User not found");
+    }
+}
