0d0dc77bd5d03da9be707fb1eb21458d58b3f486
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-25 16:32:40 -0300

    feat: remove unnecessary annotations in User entity

--- src/main/java/com/admrestaurant/backend/entities/User.java
+++ src/main/java/com/admrestaurant/backend/entities/User.java
@@ -14,15 +14,13 @@ import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
