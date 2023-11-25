701f1449873dfc8f09b052d059e6e0655c14f5ad
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-25 04:04:53 -0300

    feat: add validation of findByUserId by SolicitationRepository to not include canceled requests

--- src/main/java/com/admrestaurant/backend/repository/SolicitationRepository.java
+++ src/main/java/com/admrestaurant/backend/repository/SolicitationRepository.java
@@ -3,7 +3,13 @@ package com.admrestaurant.backend.repository;
 import com.admrestaurant.backend.entities.Solicitation;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SolicitationRepository extends JpaRepository<Solicitation, Long> {
+
+  @Query("""
+      SELECT s FROM Solicitation s
+        WHERE s.user.id = :userId and s.solicitationStatus != 'CANCELED'
+      """)
   List<Solicitation> findByUserId(Long userId);
 }
