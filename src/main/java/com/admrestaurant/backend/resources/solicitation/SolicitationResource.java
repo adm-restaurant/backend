d4a4ef733d3fec320d731e988b80bf2e192f76c6
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-25 04:12:39 -0300

    feat: create crud endpoints for solicitation module

--- src/main/java/com/admrestaurant/backend/resources/solicitation/SolicitationResource.java
+++ src/main/java/com/admrestaurant/backend/resources/solicitation/SolicitationResource.java
@@ -4,7 +4,11 @@ import com.admrestaurant.backend.entities.Solicitation;
 import com.admrestaurant.backend.services.solicitation.SolicitationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +22,26 @@ public class SolicitationResource {
   @GetMapping
   public List<Solicitation> findByCurrentUserId() {
     return this.solicitationService.findByCurrentUserId();
+  }
+
+  @PostMapping
+  public Solicitation create(
+      @RequestBody Solicitation solicitation
+  ) {
+    return this.solicitationService.create(solicitation);
+  }
+
+  @PutMapping
+  public Solicitation update(
+      @RequestBody Solicitation solicitation
+  ) {
+    return this.solicitationService.update(solicitation);
+  }
+
+  @DeleteMapping("/{id}")
+  public void cancel(
+      @RequestBody Long id
+  ) {
+    this.solicitationService.cancel(id);
   }
 }
--- src/main/java/com/admrestaurant/backend/services/solicitation/SolicitationService.java
+++ src/main/java/com/admrestaurant/backend/services/solicitation/SolicitationService.java
@@ -2,10 +2,13 @@ package com.admrestaurant.backend.services.solicitation;
 
 import com.admrestaurant.backend.common.SecurityUtils;
 import com.admrestaurant.backend.entities.Solicitation;
+import com.admrestaurant.backend.entities.SolicitationStatus;
 import com.admrestaurant.backend.repository.SolicitationRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +17,25 @@ public class SolicitationService {
 
   public List<Solicitation> findByCurrentUserId() {
     return solicitationRepository.findByUserId(SecurityUtils.getCurrentUserId());
+  }
+
+  public Solicitation update(Solicitation solicitation) {
+    return solicitationRepository.save(solicitation);
+  }
+
+  public Solicitation create(Solicitation solicitation) {
+    return solicitationRepository.save(solicitation);
+  }
+
+  public void cancel(Long id) {
+    Solicitation solicitation = solicitationRepository.findById(id).orElseThrow(
+        () -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "Solicitation not found with id: " + id
+        ) {
+        }
+    );
+    solicitation.setSolicitationStatus(SolicitationStatus.CANCELED);
+    solicitationRepository.save(solicitation);
   }
 }
