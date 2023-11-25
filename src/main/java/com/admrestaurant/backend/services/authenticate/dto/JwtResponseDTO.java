e18d6ce3f1d68658c1ebcb11810652d72ef962a6
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-25 17:46:22 -0300

    feat: extend UserDTO in JwtResponseDTO

--- src/main/java/com/admrestaurant/backend/services/authenticate/dto/JwtResponseDTO.java
+++ src/main/java/com/admrestaurant/backend/services/authenticate/dto/JwtResponseDTO.java
@@ -1,13 +1,22 @@
 package com.admrestaurant.backend.services.authenticate.dto;
 
+import com.admrestaurant.backend.entities.Role;
+import com.admrestaurant.backend.resources.user.dto.UserDTO;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @RequiredArgsConstructor
-@Builder
-public class JwtResponseDTO {
-	private final Long id;
+public class JwtResponseDTO extends UserDTO {
 	private final String token;
+
+	@Builder
+	public JwtResponseDTO(Long id, String name, String password,
+			Role role, String token) {
+		super(id, name, password, role);
+		this.token = token;
+	}
 }
