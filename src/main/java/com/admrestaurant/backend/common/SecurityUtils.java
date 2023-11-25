41168e10ef30c33fb86fd49897daa60051bd5d72
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-25 17:46:07 -0300

    feat: create method for get current user in SecurityUtils

--- src/main/java/com/admrestaurant/backend/common/SecurityUtils.java
+++ src/main/java/com/admrestaurant/backend/common/SecurityUtils.java
@@ -4,6 +4,14 @@ import com.admrestaurant.backend.entities.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
+
+    public static User getCurrentUser() {
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User) {
+            return ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        }
+        throw new RuntimeException("User not found");
+    }
+
     public static String getCurrentUsername() {
         if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User) {
             return ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
