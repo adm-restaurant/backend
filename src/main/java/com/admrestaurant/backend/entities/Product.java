a4f22c8a16461d29db2a0efd9e74317b08e3930a
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-26 15:34:58 -0300

    feat: change type in column categoria of entity Product

--- src/main/java/com/admrestaurant/backend/entities/Product.java
+++ src/main/java/com/admrestaurant/backend/entities/Product.java
@@ -2,6 +2,8 @@ package com.admrestaurant.backend.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -33,5 +35,6 @@ public class Product {
     private Double price;
 
     @Column(name = "categoria")
+    @Enumerated(EnumType.STRING)
     private Category category;
 }
