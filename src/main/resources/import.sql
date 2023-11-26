51bb3b7b2fe687f01a7612d529e4151a30f9edcf
Tr00vuada <gabrielmiguel651@gmail.com>
2023-11-25 21:49:21 -0300

    Changes into import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,9 +5,9 @@ insert into public.produto (id, nome, preco, categoria) values (3, 'Fanta', 5.00
 insert into public.produto (id, nome, preco, categoria) values (4, 'Sprite', 5.00, 'BEBIDAS');
 
 -- ALIMENTAÇÃO
-insert into public.produto (id, nome, preco, categoria) values (5, 'Arroz', 5.00, 'ALIMENTACAO');
-insert into public.produto (id, nome, preco, categoria) values (6, 'Feijão', 5.00, 'ALIMENTACAO');
-insert into public.produto (id, nome, preco, categoria) values (7, 'Macarrão', 5.00, 'ALIMENTACAO');
-insert into public.produto (id, nome, preco, categoria) values (8, 'Carne', 5.00, 'ALIMENTACAO');
-insert into public.produto (id, nome, preco, categoria) values (9, 'Frango', 5.00, 'ALIMENTACAO');
-insert into public.produto (id, nome, preco, categoria) values (10, 'Peixe', 5.00, 'ALIMENTACAO');
+insert into public.produto (id, nome, preco, categoria) values (5, 'X-salada', 5.00, 'ALIMENTACAO');
+insert into public.produto (id, nome, preco, categoria) values (6, 'X-frango', 5.00, 'ALIMENTACAO');
+insert into public.produto (id, nome, preco, categoria) values (7, 'X-bacon', 5.00, 'ALIMENTACAO');
+insert into public.produto (id, nome, preco, categoria) values (8, 'X-tudo', 5.00, 'ALIMENTACAO');
+insert into public.produto (id, nome, preco, categoria) values (9, 'X-calota', 5.00, 'ALIMENTACAO');
+insert into public.produto (id, nome, preco, categoria) values (10, 'X-coração', 5.00, 'ALIMENTACAO');
